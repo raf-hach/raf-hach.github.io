0f478d4440cb026d4cf9ec00f1e7ea5912afa654
pavel1787mego <pavel1787mego@gmail.com>
2017-10-14 13:50:59 +0300

    Test

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
-<?
-	echo '<pre>';
-	print_r($_SERVER);
-	echo '</pre>';
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Раф Хачатурян</title>
+</head>
+<body>
+	<h1>Раф Хачатурян - история маленького пидapa</h1>
+	<img src="https://pp.userapi.com/c841027/v841027772/2bc7a/lAMpXglcw3g.jpg">
+	<a href="https://vk.com/iveta1967">Его мамка</a>
+	<img src="https://iplogger.com/1ZLu27" title="iplogger.com - IP Logging Service" border="0">
+</body>
+</html>
